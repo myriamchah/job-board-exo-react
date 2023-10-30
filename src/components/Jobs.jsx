92ad47dd53fa51a3aa772105b7edfb6928485124
myriam chahlafi <myriam.chah@gmail.com>
2023-10-30 16:40:21 +0100

    Randomize border colors

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,68 +1,73 @@
 import { faker } from "@faker-js/faker";
 import Job from "./Job";
 
+const color = () => {
+  const colors = ["purple", "pink", "green", "blue", "yellow"];
+  return colors[Math.floor(Math.random() * 5)];
+};
+
 const Jobs = (props) => {
   return (
     <>
       <div className={props.layout}>
         <Job
-          color="purple"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDI"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="pink"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDI"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="green"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDD"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="pink"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDD"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="blue"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDI"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="cyan"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDI"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="pink"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDI"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="green"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDI"
           country={faker.location.country()}
           city={faker.location.city()}
         />
         <Job
-          color="purple"
+          color={color()}
           title={faker.person.jobTitle()}
           contractType="CDD"
           country={faker.location.country()}
